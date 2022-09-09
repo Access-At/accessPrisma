198869a9c6e98c6af8c5d56e90d35b282eb9877e
rizsyad <rizsyad@gmail.com>
2022-09-09 11:44:53 +0700

    add save, comment, detail thread, fix delete

--- src/api/models/ThreadModel.ts
+++ src/api/models/ThreadModel.ts
@@ -1,35 +1,86 @@
-import { validationThreadCreate, validationThreadDelete, validationThreadLike, validationThreadUpdate } from './../validations/ThreadValidation'
-import prisma from '../../../prisma'
-import { validationThread } from '../validations/ThreadValidation'
+import {
+  validationThreadCreate,
+  validationThreadDelete,
+  validationThreadLike,
+  validationThreadUpdate,
+  validationThreadSave,
+  validationThreadComment,
+  validationThreadDetail,
+} from "./../validations/ThreadValidation";
+import prisma from "../../../prisma";
+import { validationThread } from "../validations/ThreadValidation";
 
 export const thread = async (skip: number) => {
-  if ((await validationThread(skip)) === -1) return 'Posts is empty'
+  if ((await validationThread(skip)) === -1) return "Posts is empty";
 
   const thread = await prisma.thread.findMany({
     skip,
     take: 12,
-    orderBy: { createAt: 'desc' },
-    include: { author: { select: { displayName: true } }, _count: { select: { commentThread: true, saveThread: true, likeThread: true } } },
-  })
-  return thread
-}
+    orderBy: { createAt: "desc" },
+    include: {
+      author: { select: { displayName: true } },
+      _count: {
+        select: { commentThread: true, saveThread: true, likeThread: true },
+      },
+    },
+  });
+  return thread;
+};
+
+export const threadDetail = async (id: string) => {
+  if ((await validationThreadDetail(id)) === -1) return "Thread is empty";
+
+  const thread = await prisma.thread.findFirst({
+    where: { id },
+    include: {
+      author: { select: { displayName: true } },
+      commentThread: {
+        select: {
+          description: true,
+          createAt: true,
+          commentBy: { select: { displayName: true, username: true } },
+        },
+      },
+      _count: {
+        select: { commentThread: true, saveThread: true, likeThread: true },
+      },
+    },
+  });
+
+  // const thread = await prisma.thread.findMany({
+  //   skip,
+  //   take: 12,
+  //   orderBy: { createAt: "desc" },
+  //   include: {
+  //     author: { select: { displayName: true } },
+  //     _count: {
+  //       select: { commentThread: true, saveThread: true, likeThread: true },
+  //     },
+  //   },
+  // });
+  return thread;
+};
 
 export const threadCreate = async (authorId: string, description: string) => {
-  if ((await validationThreadCreate(authorId, description)) === -1) return "Description can't be empty"
+  if ((await validationThreadCreate(authorId, description)) === -1)
+    return "Description can't be empty";
 
   const threadCreate = await prisma.thread.create({
     data: {
       authorId,
       description,
     },
-  })
-  return threadCreate
-}
+  });
+  return threadCreate;
+};
 
 export const threadLikes = async (threadId: string, userId: string) => {
-  if ((await validationThreadLike(threadId, userId)) === -1) return "Can't be empty"
-  if ((await validationThreadLike(threadId, userId)) === -2) return "Can't find thread"
-  if ((await validationThreadLike(threadId, userId)) === -3) return "Can't find user"
+  if ((await validationThreadLike(threadId, userId)) === -1)
+    return "Can't be empty";
+  if ((await validationThreadLike(threadId, userId)) === -2)
+    return "Can't find thread";
+  if ((await validationThreadLike(threadId, userId)) === -3)
+    return "Can't find user";
   if ((await validationThreadLike(threadId, userId)) === -4) {
     const disLike = await prisma.likeThread.deleteMany({
       where: {
@@ -42,8 +93,8 @@ export const threadLikes = async (threadId: string, userId: string) => {
           },
         ],
       },
-    })
-    return disLike
+    });
+    return disLike;
   }
 
   const threadLike = await prisma.likeThread.create({
@@ -51,26 +102,92 @@ export const threadLikes = async (threadId: string, userId: string) => {
       threadId,
       userId,
     },
-  })
-  return threadLike
-}
+  });
+  return threadLike;
+};
 
-export const threadUpdate = async (threadId: string, authorId: string, description: string) => {
-  // if ((await validationThreadUpdate(threadId, authorId)) === -1) return "threadId and authorId can't be empty"
-  // if ((await validationThreadUpdate(threadId, authorId)) === -2) return "threadId can't find"
-  // if ((await validationThreadUpdate(threadId, authorId)) === -3) return "userId can't find"
-  // const update = await prisma.thread.update({
-  //   where: { id: threadId },
-  //   data: {
-  //     description,
-  //   },
-  // })
-  // console.log(update)
-}
+export const threadSave = async (threadId: string, userId: string) => {
+  if ((await validationThreadSave(threadId, userId)) === -1)
+    return "Can't be empty";
+  if ((await validationThreadSave(threadId, userId)) === -2)
+    return "Can't find thread";
+  if ((await validationThreadSave(threadId, userId)) === -3)
+    return "Can't find user";
+
+  if ((await validationThreadSave(threadId, userId)) === -4) {
+    const disSave = await prisma.saveThread.deleteMany({
+      where: {
+        AND: [
+          {
+            threadId,
+          },
+          {
+            userId,
+          },
+        ],
+      },
+    });
+    return disSave;
+  }
+
+  const threadSave = await prisma.saveThread.create({
+    data: {
+      threadId,
+      userId,
+    },
+  });
+  return threadSave;
+};
+
+export const threadUpdate = async (
+  threadId: string,
+  authorId: string,
+  description: string
+) => {
+  if ((await validationThreadUpdate(threadId, authorId, description)) === -1)
+    return "threadId, authorId, description can't be empty";
+  if ((await validationThreadUpdate(threadId, authorId, description)) === -2)
+    return "threadId can't find";
+  if ((await validationThreadUpdate(threadId, authorId, description)) === -3)
+    return "userId can't find";
+  const update = await prisma.thread.update({
+    where: { id: threadId },
+    data: {
+      description,
+    },
+  });
+
+  return update;
+};
 
 export const threadDelete = async (threadId: string, authorId: string) => {
-  if ((await validationThreadDelete(threadId, authorId)) === -1) return 'data ini gada!!!'
+  if ((await validationThreadDelete(threadId, authorId)) === -1)
+    return "threadId and authorId can't be empty";
+  if ((await validationThreadDelete(threadId, authorId)) === -2)
+    return "threadId can't find";
+  if ((await validationThreadDelete(threadId, authorId)) === -3)
+    return "userId can't find";
 
-  const deleted = await prisma.thread.delete({ where: { id: threadId } })
-  return deleted
-}
+  const deleted = await prisma.thread.delete({ where: { id: threadId } });
+  return deleted;
+};
+
+export const threadComment = async (
+  threadId: string,
+  userId: string,
+  description: string
+) => {
+  if ((await validationThreadComment(threadId, userId, description)) === -1)
+    return "threadId, authorId, description can't be empty";
+  if ((await validationThreadComment(threadId, userId, description)) === -2)
+    return "threadId can't find";
+
+  const threadComment = await prisma.commentThread.create({
+    data: {
+      threadId,
+      userId,
+      description,
+    },
+  });
+  return threadComment;
+};
--- src/api/routes/ThreadRoute.ts
+++ src/api/routes/ThreadRoute.ts
@@ -1,13 +1,26 @@
-import { Router } from 'express'
-import { getAllThread, CreateThread, LikeThread, UpdateThread, DeleteThread } from '../controllers/ThreadController'
-import isAuthorized from '../middlewares/isAuthorized'
+import { Router } from "express";
+import {
+  getAllThread,
+  CreateThread,
+  LikeThread,
+  UpdateThread,
+  DeleteThread,
+  SaveThread,
+  CommentThread,
+  detailThread,
+} from "../controllers/ThreadController";
+import isAuthorized from "../middlewares/isAuthorized";
 
-const route = Router()
+const route = Router();
 
-route.get('/thread/:skip?', isAuthorized, getAllThread)
-route.post('/thread/create', isAuthorized, CreateThread)
-route.post('/thread/like', isAuthorized, LikeThread)
-route.put('/thread/update', isAuthorized, UpdateThread)
-route.delete('/thread/delete', isAuthorized, DeleteThread)
+route.post("/thread/create", isAuthorized, CreateThread);
+route.get("/thread/page/:skip?", isAuthorized, getAllThread);
+route.get("/thread/:id", isAuthorized, detailThread);
+route.put("/thread/update", isAuthorized, UpdateThread);
+route.delete("/thread/delete", isAuthorized, DeleteThread);
+route.post("/thread/like", isAuthorized, LikeThread);
+route.post("/thread/comment", isAuthorized, CommentThread);
+route.post("/thread/save", isAuthorized, SaveThread);
+// route.post("/thread/subscribe", isAuthorized, SubscribeThread);
 
-export default route
+export default route;
