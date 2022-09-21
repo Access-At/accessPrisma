3ab06d27ffc08332eca05abe16e64aaec27a3367
bayupb <kampuspb.bayu@gmail.com>
2022-09-21 16:49:22 +0700

    add user

--- src/api/query/thread/ThreadAll.ts
+++ src/api/query/thread/ThreadAll.ts
@@ -24,23 +24,4 @@ export const allThreadQuery = async (res: any, req: any, skip: number) => {
 		{ page: skip }
 	);
 	return result;
-
-	// const thread = await prisma.thread.findMany({
-	// 	skip,
-	// 	take: 12,
-	// 	orderBy: { createAt: "desc" },
-	// 	include: {
-	// 		author: { select: { displayName: true, username: true } },
-	// 		_count: {
-	// 			select: { commentThread: true, saveThread: true, likeThread: true },
-	// 		},
-	// 		likeThread: {
-	// 			select: { userId: true },
-	// 		},
-	// 		saveThread: {
-	// 			select: { userId: true },
-	// 		},
-	// 	},
-	// });
-	// return thread;
 };
